de08df21270ad50698951d2ea7d106477c782a76
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-03-31 21:59:47 +0200

    define uniffi custom type conversions for external types

---
+++ src/uniffi_custom_types.rs
@@ -0,0 +1,35 @@
+use std::path::PathBuf;
+use reqwest::Url;
+
+use crate::UniffiCustomTypeConverter;
+// Custom UniFFI types
+
+// `Url` as a custom type, with `String` as the Builtin
+uniffi::custom_type!(Url, String);
+
+impl UniffiCustomTypeConverter for Url {
+    type Builtin = String;
+
+    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+        val.parse::<Url>().map_err(|e | {e.into()})
+    }
+
+    fn from_custom(obj: Self) -> Self::Builtin {
+        obj.as_str().to_owned()
+    }
+}
+
+// `PathBuf` as a custom type, with `String` as the Builtin
+uniffi::custom_type!(PathBuf, String);
+
+impl UniffiCustomTypeConverter for PathBuf {
+    type Builtin = String;
+
+    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+        Ok(PathBuf::from(val))
+    }
+
+    fn from_custom(obj: Self) -> Self::Builtin {
+        format!("{:?}", obj.display())
+    }
+}
