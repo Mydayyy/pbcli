b694ceb353c15eac5f1712d0d55bb26158480cc3
Mydayyy <mydayyy@mydayyy.eu>
2024-07-26 00:03:53 +0200

    conditionally include scaffolding

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ pub mod error;
 pub mod opts;
 pub mod privatebin;
 pub mod util;
+
+#[cfg(feature = "uniffi")]
 mod uniffi_custom_types;
 
 pub use api::API;
@@ -13,4 +15,5 @@ pub use opts::Opts;
 pub use privatebin::{DecryptedPaste, PasteFormat};
 pub use util::check_filesize;
 
+#[cfg(feature = "uniffi")]
 uniffi::setup_scaffolding!();
